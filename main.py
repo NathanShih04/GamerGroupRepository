6a659ed529399e1d0cb2d42692e6ecdd14232241
Naevey <fpsgaaa@gmail.com>
2021-09-23 14:30:45 -0700

    tried adding binary things from today, not so sure that it works.

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from flask import Flask, render_template, request
 # create a Flask instance
 app = Flask(__name__)
 
+
 # connects default URL to render index.html
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/Turtle Clicker/')
 def turtle():
     return render_template("turtleclicker.html")
+
 
 # connects /kangaroos path to render kangaroos.html
 @app.route('/kurtis/')
@@ -23,9 +26,11 @@ def kurtis():
 def nathan():
     return render_template("nathan.html")
 
+
 @app.route('/colin/')
 def colin():
     return render_template("colin.html")
+
 
 @app.route('/everitt/')
 def everitt():
@@ -36,6 +41,7 @@ def everitt():
 def jackson():
     return render_template("jackson.html")
 
+
 @app.route('/greet', methods=['GET', 'POST'])
 def greet():
     # submit button has been pushed
@@ -45,6 +51,8 @@ def greet():
             return render_template("greet/greet.html", name=name)
     # starting and empty input default
     return render_template("greet/greet.html", name="World")
+
+
 @app.route('/jgreet', methods=['GET', 'POST'])
 def jgreet():
     # submit button has been pushed
@@ -54,6 +62,8 @@ def jgreet():
             return render_template("greet/jacksongreet.html", name=name)
     # starting and empty input default
     return render_template("greet/jacksongreet.html", name="World")
+
+
 @app.route('/ngreet', methods=['GET', 'POST'])
 def ngreet():
     # submit button has been pushed
@@ -63,6 +73,8 @@ def ngreet():
             return render_template("greet/nathangreet.html", name=name)
     # starting and empty input default
     return render_template("greet/nathangreet.html", name="World")
+
+
 @app.route('/egreet', methods=['GET', 'POST'])
 def egreet():
     # submit button has been pushed
@@ -72,6 +84,8 @@ def egreet():
             return render_template("greet/everittgreet.html", name=name)
     # starting and empty input default
     return render_template("greet/everittgreet.html", name="World")
+
+
 @app.route('/cgreet', methods=['GET', 'POST'])
 def cgreet():
     # submit button has been pushed
@@ -81,32 +95,56 @@ def cgreet():
             return render_template("greet/colingreet.html", name=name)
     # starting and empty input default
     return render_template("greet/colingreet.html", name="World")
+
+
 @app.route('/kabout/')
 def kabout():
     return render_template("about/kurtisabout.html")
+
+
 @app.route('/eabout/')
 def eabout():
     return render_template("about/everittabout.html")
+
+
 @app.route('/jabout/')
 def jabout():
     return render_template("about/jacksonabout.html")
+
+
 @app.route('/nabout/')
 def nabout():
     return render_template("about/nathanabout.html")
+
+
 @app.route('/cabout/')
 def cabout():
     return render_template("about/colinabout.html")
 
-@app.route('/clicker/')
+
+@app.route('/clicker/', methods=['GET', 'POST'])
 def clicker():
-    return render_template("clicker.html")
+    if request.method == "POST":
+        if request.form:
+            bitNumber = request.form.get("bits")
+            if len(bitNumber) != 0:
+                return render_template("clicker.html", BITS=int(bitNumber), imageOn="/static/turtleon!.jpg",  imageOff="turtledefault.png")
+        if request.form["bits2"]:
+            return render_template("clicker.html", BITS=8, imageOn="/static/turtleon!.jpg",
+                                   imageOff="/static/turtledefault.png")
+        return render_template("clicker.html", BITS=8, imageOn="/static/turtleon!.jpg",
+                               imageoff="/static/turtleoff.jpg")
+
 
 @app.route('/tclicker/')
 def tclicker():
     return render_template("turtleclicker.html")
+
+
 @app.route('/rgb/')
 def rgb():
     return render_template("labs/rgb.html")
+
 
 # runs the application on the development server
 if __name__ == "__main__":
