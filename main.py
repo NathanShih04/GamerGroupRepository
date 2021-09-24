bc71c24b11c1ff636b2d753c8095b35e74936a93
Naevey <fpsgaaa@gmail.com>
2021-09-24 15:01:43 -0700

    rgb lab basically works now

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 # import "packages" from flask
 from flask import Flask, render_template, request
 from image import image_data
-from pathlib import \
-    Path  # https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
 
+from pathlib import Path  # https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
 # create a Flask instance
 app = Flask(__name__)
 
@@ -147,7 +146,7 @@ def tclicker():
 
 @app.route('/rgb/')
 def rgb():
-    path = Path(app.root_path) / "static" / "img"
+    path = Path(app.root_path) / "static" / "assets"
     return render_template('labs/rgb.html', images=image_data(path))
 
 
