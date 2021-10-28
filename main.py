eead77c5353053a99c95e4702a68a57e41c1fd56
Naevey <fpsgaaa@gmail.com>
2021-10-28 14:43:10 -0700

    "marine api"

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-2# import "packages" from flask
+import json
+
+from flask_restful.inputs import url
+
+2  # import "packages" from flask
 from flask import Flask, render_template, request
 from image import image_data
+import requests
+from pathlib import \
+    Path  # https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
 
-from pathlib import Path  # https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
 # create a Flask instance
 app = Flask(__name__)
 
@@ -129,7 +135,7 @@ def cabout():
     return render_template("about/colinabout.html")
 
 
-@app.route("/binary/", methods = ['GET', 'POST'])
+@app.route("/binary/", methods=['GET', 'POST'])
 def binary():
     BITS = 8
     imgBulbOn = "/static/turtleon!.png"
@@ -139,28 +145,35 @@ def binary():
         imgBulbOn = request.form['lightOn']
     return render_template("clicker.html", imgBulbOn=imgBulbOn, BITS=BITS)
 
+
 @app.route('/tclicker/')
 def tclicker():
     return render_template("turtleclicker.html")
 
+
 @app.route('/mreaction/')
 def mreaction():
     return render_template("monkeyreaction/monkeyreaction.html")
+
+
 @app.route('/unsignedaddition/')
 def unsignedaddition():
     return render_template("/labs/unsignedaddition.html")
+
 
 @app.route('/rgb/')
 def rgb():
     path = Path(app.root_path) / "static" / "assets"
     return render_template('labs/rgb.html', images=image_data(path))
 
+
 @app.route('/logicgates/')
 def logicgates():
     path = Path(app.root_path) / "static" / "assets"
     return render_template('labs/logicgates.html', images=image_data(path))
 
-@app.route("/colorcode/", methods = ['GET', 'POST'])
+
+@app.route("/colorcode/", methods=['GET', 'POST'])
 def ccode():
     BITS = 8
     imgBulbOn = "/static/turtleon!.png"
@@ -169,6 +182,20 @@ def ccode():
         BITS = int(request.form['BITS'])
         imgBulbOn = request.form['lightOn']
     return render_template("/labs/colorcode.html", imgBulbOn=imgBulbOn, BITS=BITS)
+
+@app.route('/marine/',methods=['GET','POST'])
+def study():
+
+    url = "https://trivia-by-api-ninjas.p.rapidapi.com/v1/trivia"
+    querystring = {"category":"sciencenature","limit":"1"}
+    headers = {
+        'x-rapidapi-host': "trivia-by-api-ninjas.p.rapidapi.com",
+        'x-rapidapi-key': "6279ac9b7amsh7dc015c7d7746fbp1f4d65jsn125b0c500438"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    output = json.loads(response.text)
+    return render_template('labs/marine.html', question=output)
+
 # runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True)
